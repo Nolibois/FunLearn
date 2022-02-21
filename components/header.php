22cf3379631a54ae2795ad17d4b084765dc22875
Olivier Nolibois <olivier.nolibois@gmail.com>
2022-02-21 11:28:24 +0100

    init

---
+++ components/header.php
@@ -0,0 +1,14 @@
+<header>
+<nav>
+  <div class="logo">
+    <img src="" alt="Logo FunLearn">
+    <p>Tu participes, tu gagnes.</p>
+  </div>
+  <ul>
+    <li><a href="./index.php">Accueil</a></li>
+    <li><a href="./pages/participant.php">Prticipant</a></li>
+    <li><a href="./pages/modul.php">Modules</a></li>
+    <li><a href="">Exports</a></li>
+  </ul>
+</nav>
+</header>
